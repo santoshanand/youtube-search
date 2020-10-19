eb84954108450366af8f8805552010a7a32fc80a
Santosh Anand <santoshanand.jogi@gmail.com>
2020-10-19 10:12:53 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,50 @@
 const puppeteer = require('puppeteer');
+
+
+
+
+async function YoutubeSearch(searchText) {
+  let videos = [];
+  let browser = await puppeteer.launch({ headless: true });
+  let page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1080 });
+  await page.setRequestInterception(true);
+  
+  page.on('request', (req) => {
+      if(req.resourceType() == 'stylesheet' || req.resourceType() == 'font'){
+          req.abort();
+      }
+      else {
+          req.continue();
+      }
+  });
+  
+  await page.goto('https://www.youtube.com/results?search_query='+searchText, {waitUntil: 'networkidle0'});
+  await page.waitForSelector('ytd-video-renderer');
+  const elHandleArray = await page.$$('.ytd-item-section-renderer');
+    for (let index = 0; index < elHandleArray.length; index++) {
+      const el = elHandleArray[index];
+      try {
+        const videoId = await el.$eval('.ytd-thumbnail', a => a.getAttribute('href') );
+        const linkThumb = await el.$eval('.yt-img-shadow', a => a.getAttribute('src'));
+        const title = await el.$eval('#video-title', a => a.getAttribute('title'));
+        videos.push({videoId: videoId, linkThumb: linkThumb, title: title});
+      }catch(e) {}
+    }
+    await page.close();
+    await browser.close();
+    return videos;
+}
+
+
+/*
 async function YoutubeSearch(searchText) {
   let videos = [];
   try {
     const browserFetcher = puppeteer.createBrowserFetcher();
     const revisionInfo = await browserFetcher.download('639839');
     const browser = await puppeteer.launch({
-      headless: true,
+      headless: false,
       executablePath: revisionInfo.executablePath,
       args: ['--window-size=1920,1080', '--disable-notifications'],
       userDataDir: './chrome-data',
@@ -17,7 +56,7 @@ async function YoutubeSearch(searchText) {
     });
     // await page.goto('https://youtube.com', {
     //   waitUntil: 'networkidle0',
-    // });
+    // });hyb 
     await page.goto('https://www.youtube.com/results?search_query='+searchText, {waitUntil: 'networkidle0'});
     await page.waitForSelector('ytd-video-renderer');
     await page.evaluate('window.scrollTo(0, 1000)')
@@ -39,5 +78,8 @@ async function YoutubeSearch(searchText) {
     return videos;
   } catch (e) {}
 };
+*/
 
 module.exports = {YoutubeSearch: YoutubeSearch}
+
+YoutubeSearch('demo')
