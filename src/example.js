30b34c6f4b7936a36fdaf10050e38aa2ebb3afa4
Santosh Anand <santoshanand.jogi@gmail.com>
2020-10-24 14:05:55 +0530

    Added

--- src/example.js
+++ src/example.js
@@ -4,5 +4,11 @@ InitBrowser().then(async (res) => {
   const videos = await YoutubeSearch('rihana')
   const videoss = await YoutubeSearch('golang')
   console.log('res ', videos);
-  console.log('res ', videoss);
+  console.log('ress ', videoss);
+
+  // setInterval(async() => {
+  //   const videoss = await YoutubeSearch('test')
+  //   console.log('ress ', videoss);
+    
+  // }, 5000);
 });
--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const puppeteer = require('puppeteer');
 let browserInit = false;
 let page = null;
 let browser = null;
-async function InitBrowser() {
+async function InitBrowser(path) {
   return new Promise(async(resolve, reject) => {
     if(!browserInit) {
       browserInit = true;
-      browser = await puppeteer.launch({ headless: true });
+      browser = await puppeteer.launch({ headless: true});
       page = await browser.newPage();
       await page.setViewport({ width: 1920, height: 1080 });
       await page.setRequestInterception(true);
